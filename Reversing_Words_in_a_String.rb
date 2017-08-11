1c06618616c0b5858d0296e9b0419bb46b91522d
zhangruochi <zrc720@gmail.com>
2017-08-12 02:36:27 +0800

    2017-08-12

--- Reversing_Words_in_a_String.rb
+++ Reversing_Words_in_a_String.rb
@@ -4,6 +4,16 @@
 #-name   : zhangruochi
 #-email  : zrc720@gmail.com
 
+=begin
+You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+reverse('Hello World') == 'World Hello'
+reverse('Hi There.') == 'There. Hi'
+=end
+
+
 def reverse(string)
   #your code here
   return string.split.reverse.join(" ")
