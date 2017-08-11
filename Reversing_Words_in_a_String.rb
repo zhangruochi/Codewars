9b0c36740a69f1cfded419f1ed20baff1d73a5ae
zhangruochi <zrc720@gmail.com>
2017-08-12 02:35:18 +0800

    2017-08-12

---
+++ Reversing_Words_in_a_String.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+#info
+#-name   : zhangruochi
+#-email  : zrc720@gmail.com
+
+def reverse(string)
+  #your code here
+  return string.split.reverse.join(" ")
+end
+
+
+p " ".split
+p [].join(" ")
